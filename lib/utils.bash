0b9195fba2f0e81180f0dc166756e59756aa6ef8
Jason McVetta <jason.mcvetta@gmail.com>
2022-05-24 19:35:20 -0400

    feat: adapt template for Terraformer

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for terraformer.
 GH_REPO="https://github.com/GoogleCloudPlatform/terraformer"
 TOOL_NAME="terraformer"
 TOOL_TEST="terraformer --version"
@@ -31,8 +30,6 @@ list_github_tags() {
 }
 
 list_all_versions() {
-  # TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-  # Change this function if terraformer has other means of determining installable versions.
   list_github_tags
 }
 
@@ -41,8 +38,8 @@ download_release() {
   version="$1"
   filename="$2"
 
-  # TODO: Adapt the release URL convention for terraformer
-  url="$GH_REPO/archive/v${version}.tar.gz"
+  # https://github.com/GoogleCloudPlatform/terraformer/releases/download/0.8.20/terraformer-all-linux-amd64
+  url="$GH_REPO/releases/download/${version}/terraformer-all-linux-amd64"
 
   echo "* Downloading $TOOL_NAME release $version..."
   curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
@@ -61,7 +58,6 @@ install_version() {
     mkdir -p "$install_path"
     cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-    # TODO: Asert terraformer executable exists.
     local tool_cmd
     tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
     test -x "$install_path/bin/$tool_cmd" || fail "Expected $install_path/bin/$tool_cmd to be executable."
